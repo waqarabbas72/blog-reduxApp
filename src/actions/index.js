7a5122d9992ba4370d950f0a7345bc4b77814e1d
waqarabbas72 <sheraliate751@gmail.com>
2023-05-02 20:22:47 +0500

    chain method

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,8 +3,12 @@ import jsonPlaceholders from '../apis/jsonPlaceholders'
 
 export const fetchPostsAndUsers = () => async (dispatch , getState) => {
    await dispatch(fetchPosts())
-    const userIds = _.uniq(_.map(getState().posts , 'userId'))
-    userIds.forEach(id => (fetchUser(id)))
+        
+    _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value()
 } 
 
 export const fetchPosts = () => async(dispatch) => {
